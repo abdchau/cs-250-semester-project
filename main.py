6d931dc639f33d3d874529089a16646d908853aa
Abdullah Chaudhry <abdullahchaudhry0@outlook.com>
2019-11-09 22:14:35 +0500

    Building a simple lexicon from just one file

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import json
 import os
+#import nltk
+import string
 
-
-inDir = "D:\\Uni\\Semester 3\\DSA\\Project\\Popular Blog Post Dataset\\717_webhose-2017-03_20170904123310"
+inDir = "..\\Popular Blog Post Dataset\\717_webhose-2017-03_20170904123310"
 
 def main():
 	#dicc = 0
-	with open(os.path.join(inDir, 'blogs_0000002.json'), 'r', encoding="utf8") as f:
+	with open(os.path.join(inDir, 'blogs_0000001.json'), 'r', encoding="utf8") as f:
 		dicc = json.load(f)
 
-	#print(dicc)
-	with open('keys1.txt', 'w') as f:
-		for key in dicc:
-			f.write(key+"\n")
+	text = dicc['text']
+	tokens = text.split()
+
+
+	lexicon = dict()
+	wordID = 0
+
+	invalidChars = set(string.punctuation)
+
+	for token in tokens:
+		token = token.lower().translate(string.punctuation)
+		if not (token in lexicon): #not any(char in invalidChars for char in token)
+			lexicon[token] = wordID
+			wordID+=1
+
+	with open('lexicon.json', 'w') as f:
+		json.dump(lexicon, f)
 
 main()
