985525da511ee15a51ff08cc277f959c267e4b70
shariquep <44778457+shariquep@users.noreply.github.com>
2019-11-10 14:03:28 +0500

    Add files via upload
    efficient code for main

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 import json
 import os
-import operator
 import string
 from tqdm import tqdm
 
-inDir = "../Popular Blog Post Dataset/717_webhose-2017-03_20170904123310"
+inDir = "D:/data/717_webhose-2017-03_20170904123310"
 
 def main():
-	
-	for file in tqdm(os.listdir(inDir)[:20]):		#run for entire directory to generate complete lexicon
-		with open(inDir+file), 'r', encoding="utf8") as f:
-			dicc = json.load(f)
+        #open lexicon file to load data only once
+	try:
+		with open('lexicon.json', 'r', encoding="utf8") as lexfile:
+			lexicon = json.load(lexfile)
+		wordID = lexicon[list(lexicon.keys())[-1]] + 1		# get the last wordID in the lexicon,
+	except FileNotFoundError:
+		lexicon = dict()
+		wordID = 0            															# add 1 to get wordID for next addition
+		
+	for file in tqdm(os.listdir(inDir)[:33500]):		# run for entire directory to generate complete lexicon
+		with open(os.path.join(inDir, file), 'r', encoding="utf8") as f:
+			myDict = json.load(f)
 
-		text = dicc['text']
+		text = myDict['text']
 
+		# remove punctuation from the text. Some hardcoding for Unicode characters
 		translator = str.maketrans('', '', string.punctuation)
-		text = text.lower().translate(translator).replace('\u201c', "").replace('\u201d', "")
-		tokens = text.replace("-", " ").split()
+		text = text.lower().replace("-", " ").replace('\u201c', "").replace('\u201d', "").translate(translator)
+		tokens = text.replace('\u2018', "").replace('\u2019', "").split()
 
-		try:
-			with open('lexicon.json', 'r', encoding="utf8") as f:
-				lexicon =json.load(f)
-			wordID = max(lexicon.items(), key=operator.itemgetter(1))[1] + 1
-		except FileNotFoundError:
-			lexicon = dict()
-			wordID = 0
-
-		#print(wordID)
-		#invalidChars = set(string.punctuation)
-
+		# if a lexicon already exists, load it. Otherwise create new lexicon
+		
 		for token in tokens:
-			
-			if not token in lexicon: #not any(char in invalidChars for char in token)
+			id = lexicon.get(token)             #id is set to None of token doesnt exist
+			if id == None: 	# if the word is not already present in lexicon, add it
 				lexicon[token] = wordID
 				wordID+=1
-
-		with open('lexicon.json', 'w') as f:
-			json.dump(lexicon, f)
+		print("good scene")
+	with open('lexicon.json', 'w', encoding="utf8") as lexfile:     #write the dictionary to at the end
+		json.dump(lexicon, lexfile)
 
 main()
