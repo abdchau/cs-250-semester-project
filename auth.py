1808da4384a14f615cb29d68334256475ab7d318
shariquep <44778457+shariquep@users.noreply.github.com>
2019-11-24 01:21:56 +0500

    Add files via upload

--- auth.py
+++ auth.py
@@ -6,24 +6,50 @@ import collections
 
 inDir = "D:/data/717_webhose-2017-03_20170904123310"
 
+def getIndexPositions(listOfElements, element):
+    ''' Returns the indexes of all occurrences of give element in
+    the list- listOfElements '''
+    indexPosList = []
+    indexPos = 0
+    while True:
+        try:
+            # Search for item in list from indexPos to the end of list
+            indexPos = listOfElements.index(element, indexPos)
+            # Add the index position in list
+            indexPosList.append(indexPos)
+            indexPos += 1
+        except ValueError as e:
+            break
+ 
+    return indexPosList
+
 def main1():
 
-	try:
-		with open('D:/cs-250-semester-project/author.json', 'r', encoding="utf8") as lexfile:
-			lexicon = json.load(lexfile)
-	except FileNotFoundError:
-		lexicon = dict()
-		  		# get the last wordID in the lexicon,            															# add 1 to get wordID for next addition
+	position = dict()
+	docrepos = dict()
+	with open('lexicon2.json', 'r', encoding="utf8") as lexfile:
+		lexicon = json.load(lexfile)
+	wordID = lexicon[list(lexicon.keys())[-1]] + 1		# get the last wordID in the lexicon,            															# add 1 to get wordID for next addition
 		
-	for file in tqdm(os.listdir(inDir)[:10]):		# run for 3 files to generate doc id with words and hits
+	for file in tqdm(os.listdir(inDir)[1:3]):		# run for 3 files to generate doc id with words and hits
 		docId = inDir[-3:] + file[-11:-5]               #unique docID for every blog
 		with open(os.path.join(inDir,file),'r',encoding='utf8') as f:
 			mydict = json.load(f)
 
-		text = mydict['author']
-		id = lexicon.get(docId)             #id is set to None of token doesnt exist
-		if id == None: 	# if the word is not already present in lexicon, add it
-			lexicon[docId] = text
-	with open("author.json",'w',encoding = "utf8") as docfile:     
-		json.dump(lexicon,docfile)
+		text = mydict['text']
+
+                # remove punctuation from the text. Some hardcoding for Unicode characters
+		translator = str.maketrans('', '', string.punctuation)
+		text = text.lower().replace("-", " ").replace('\u201c', "").replace('\u201d', "").translate(translator)
+		tokens = text.replace('\u2018', "").replace('\u2019', "").split()
+		
+		for i in range(len(tokens)):
+			tokens[i] = lexicon[tokens[i]]	          #convert words to their respective wordId
+		for i in range(len(tokens)):	
+			indexposition = getIndexPositions(tokens,tokens[i])
+			indexposition.insert(0,len(indexposition))
+			position[tokens[i]] = indexposition
+		docrepos[docId] = position	
+	with open("doc2.json",'w',encoding = "utf8") as docfile:     
+		json.dump(docrepos,docfile)
 main1()
