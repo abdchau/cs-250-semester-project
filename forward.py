a98b819e3e365d5393c275765ec16295e44c2580
shariquep <44778457+shariquep@users.noreply.github.com>
2019-11-29 02:01:39 +0500

    slight change in forward index

--- forward.py
+++ forward.py
@@ -6,7 +6,7 @@ import collections
 import numpy as np
 
 #uncomment before use
-inDir = "D:/data/717_webhose-2017-03_20170904123310"
+inDir = "D:/data/717_webhose-2017-01_20170904123520"
 #inDir = "D:/Uni/Semester 3/DSA/Project/Popular Blog Post Dataset/717_webhose-2017-03_20170904123310"
 
 def getIndexPositions(listOfElements, element):
@@ -28,13 +28,17 @@ def getIndexPositions(listOfElements, element):
 
 def main1():
 
-	position = dict()
-	docrepos = dict()
 	with open('lexicon.json', 'r', encoding="utf8") as lexfile:
 		lexicon = json.load(lexfile)
 	wordID = lexicon[list(lexicon.keys())[-1]] + 1		# get the last wordID in the lexicon,            															# add 1 to get wordID for next addition
-		
-	for file in tqdm(os.listdir(inDir)[1:33500]):		# run for 3 files to generate doc id with words and hits
+	try:
+		with open('forward.json', 'r', encoding="utf8") as docfile:
+			docrepos = json.load(docfile)
+		print(docrepos)
+	except FileNotFoundError:
+		docrepos = dict()
+
+	for file in tqdm(os.listdir(inDir)[:5]):		# run for 3 files to generate doc id with words and hits
 		position = dict()
 		docId = inDir[-3:] + file[-11:-5]               #unique docID for every blog
 		with open(os.path.join(inDir,file),'r',encoding='utf8') as f:
@@ -52,8 +56,7 @@ def main1():
 		for i in range(len(tokens)):	
 			indexposition = getIndexPositions(tokens,tokens[i])		#get list with position of each element
 			indexposition.insert(0,len(indexposition))				#insert hits of each word at the beginning
-			npindexposition = np.array(indexposition)				#converting list into numpy array
-			position[tokens[i]] = npindexposition					#storing list of hits and positions against wordID
+			position[tokens[i]] = indexposition					#storing list of hits and positions against wordID
 		docrepos[docId] = position									#strong dictionary with wordID,hits and positing against docID
 	with open("forward.json",'w',encoding = "utf8") as docfile:     	#writing the dict into file
 		json.dump(docrepos,docfile)
